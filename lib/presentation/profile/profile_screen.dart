1484e9d8d05736c782faca45ed5d106b72a5fd42
joenaine <joenaine10@gmail.com>
2024-10-27 02:12:07 +0500

    sdfsdf

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -8,6 +8,7 @@ import 'package:echoapp/core/constants/app_styles.dart';
 import 'package:echoapp/core/theme/app_colors.dart';
 import 'package:echoapp/core/utils/to_date.dart';
 import 'package:echoapp/presentation/common_widgets/action_button_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,6 +22,35 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: AppColors.white,
         title: const Text('Профиль'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Удаление аккаунта'),
+                    content:
+                        const Text('Вы уверены что хотите удалить аккаунт?'),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Отмена')),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            context
+                                .read<AppBloc>()
+                                .add(AppEvent.logOut(context: context));
+                          },
+                          child: const Text('Удалить'))
+                    ],
+                  ),
+                );
+              },
+              icon: const Icon(CupertinoIcons.delete, color: AppColors.red))
+        ],
       ),
       body: BlocConsumer<ProfileBloc, ProfileState>(
         listener: (context, state) {
